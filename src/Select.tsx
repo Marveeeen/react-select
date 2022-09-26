cfa9cf7926665c525338da61b8b8f35a2239896b
Marveeen <jmvroqueee@gmail.com>
2022-09-26 21:33:17 +0800

    Add single functionality for singel select

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styles from './select.module.css'
 
 type TSelectOption = {
@@ -12,15 +13,43 @@ type TSelectProps = {
 }
 
 export function Select ({ options, value, onChange } : TSelectProps) {
+    const [isOpen, setIsopen] = useState(false)
+    const [highlightedIndex, setHighlightedIndex] = useState(0);
+
+    const handleSelectOption = (option: TSelectOption) => {
+        if (option !== value) onChange(option)
+    }
+
+    function isOptionSelected(option: TSelectOption) {
+        return option === value;
+      }
+
+    const handleSelectedAndHighlighted = (option : TSelectOption, index : number) => {
+        return `${styles.option} ${isOptionSelected(option) ? styles.selected : '' } ${index === highlightedIndex ? styles.highlighted : ""}`
+    }
     return (
-        <div className={styles.container}>
+        <div 
+            className={styles.container}
+            onClick={() => setIsopen(prev => !prev)}
+        >
             <span className={styles.value}>{value.label}</span>
             <button className={styles['clear-btn']}>&times;</button>
             <div className={styles.divider}></div>
             <div className={styles.caret}></div>
-            <ul className={styles.options}>
-                {options.map(option => (
-                    <li key={option.value} className={styles.option}>{option.label}</li>
+            <ul className={`${styles.options} ${isOpen ? styles['options-show'] : ''}`}>
+                {options.map((option: TSelectOption, index: number) => (
+                    <li 
+                        key={option.value} 
+                        onMouseEnter={() => setHighlightedIndex(index)}
+                        className={handleSelectedAndHighlighted(option, index)}
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            handleSelectOption(option)
+                            setIsopen(false)
+                        }}
+                    >
+                        {option.label}
+                    </li>
                 ))}
             </ul>
         </div>
