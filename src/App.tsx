5cbdceb20c705876038642b08dfb626f42557dbe
Marveeen <jmvroqueee@gmail.com>
2022-09-26 23:59:26 +0800

    Added Multi Select functionality

--- src/App.tsx
+++ src/App.tsx
@@ -30,14 +30,21 @@ const options = [
 
 function App() {
   const [value, setValue] = useState(options[0])
+  const [multiValue, setMultiValue] = useState([options[0]])
 
   const handleChange = (e: any) => {
     setValue(e)
   }
 
+  const handleMultiValue = (e: any) => {
+    setMultiValue(e)
+  }
+
   return (
     <>
       <Select options={options} value={value} onChange={handleChange} />
+      <br />
+      <Select multiple options={options} value={multiValue} onChange={handleMultiValue} />
     </>
   );
 }
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,57 +1,106 @@
-import { useState } from 'react';
-import styles from './select.module.css'
+import { useEffect, useState } from "react";
+import styles from "./select.module.css";
 
 type TSelectOption = {
-    label: string;
-    value: string | number;
-}
+  label: string;
+  value: string | number;
+};
+
+type TMultipleSelectProps = {
+  multiple: true;
+  value: TSelectOption[];
+  onChange: (value: TSelectOption[]) => void;
+};
+
+type TSingleSelectProps = {
+  multiple?: false;
+  value?: TSelectOption;
+  onChange: (value: TSelectOption | undefined) => void;
+};
 
 type TSelectProps = {
-    options: TSelectOption[]
-    value: TSelectOption;
-    onChange: (value: TSelectOption) => void
-}
+  options: TSelectOption[];
+} & (TSingleSelectProps | TMultipleSelectProps);
 
-export function Select ({ options, value, onChange } : TSelectProps) {
-    const [isOpen, setIsopen] = useState(false)
-    const [highlightedIndex, setHighlightedIndex] = useState(0);
+export function Select({ multiple, options, value, onChange }: TSelectProps) {
+  const [isOpen, setIsopen] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
 
-    const handleSelectOption = (option: TSelectOption) => {
-        if (option !== value) onChange(option)
-    }
+  useEffect(() => {
+    if (isOpen) setHighlightedIndex(0);
+  }, [isOpen]);
 
-    function isOptionSelected(option: TSelectOption) {
-        return option === value;
+  const handleSelectOption = (option: TSelectOption) => {
+    if (multiple) {
+      if (value.includes(option)) {
+        onChange(value.filter((o) => o !== option));
+      } else {
+        onChange([...value, option]);
       }
-
-    const handleSelectedAndHighlighted = (option : TSelectOption, index : number) => {
-        return `${styles.option} ${isOptionSelected(option) ? styles.selected : '' } ${index === highlightedIndex ? styles.highlighted : ""}`
+    } else {
+      if (option !== value) onChange(option);
     }
-    return (
-        <div 
-            className={styles.container}
-            onClick={() => setIsopen(prev => !prev)}
-        >
-            <span className={styles.value}>{value.label}</span>
-            <button className={styles['clear-btn']}>&times;</button>
-            <div className={styles.divider}></div>
-            <div className={styles.caret}></div>
-            <ul className={`${styles.options} ${isOpen ? styles['options-show'] : ''}`}>
-                {options.map((option: TSelectOption, index: number) => (
-                    <li 
-                        key={option.value} 
-                        onMouseEnter={() => setHighlightedIndex(index)}
-                        className={handleSelectedAndHighlighted(option, index)}
-                        onClick={(e) => {
-                            e.stopPropagation()
-                            handleSelectOption(option)
-                            setIsopen(false)
-                        }}
-                    >
-                        {option.label}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  };
+
+  function isOptionSelected(option: TSelectOption) {
+    return multiple ? value.includes(option) : option === value;
+  }
+
+  const handleSelectedAndHighlighted = (
+    option: TSelectOption,
+    index: number
+  ) => {
+    return `${styles.option} ${
+      isOptionSelected(option) ? styles.selected : ""
+    } ${index === highlightedIndex ? styles.highlighted : ""}`;
+  };
+
+  const renderValue = () => {
+    if (!multiple) return value?.label;
+    return value?.map((v) => (
+      <button
+        key={v.value}
+        onClick={(e) => {
+          e.stopPropagation();
+          handleSelectOption(v);
+        }}
+        className={styles["option-badge"]}
+      >
+        {v.label}
+        <span className={styles["remove-btn"]}>&times;</span>
+      </button>
+    ));
+  };
+
+  return (
+    <div
+      tabIndex={1}
+      className={styles.container}
+      onBlur={() => setIsopen(false)}
+      onClick={() => setIsopen((prev) => !prev)}
+    >
+      <span className={styles.value}>{renderValue()}</span>
+      <button className={styles["clear-btn"]}>&times;</button>
+      <div className={styles.divider}></div>
+      <div className={styles.caret}></div>
+      <ul
+        className={`${styles.options} ${isOpen ? styles["options-show"] : ""}`}
+      >
+        {options.map((option: TSelectOption, index: number) => (
+          <li
+            key={option.value}
+            onMouseEnter={() => setHighlightedIndex(index)}
+            className={handleSelectedAndHighlighted(option, index)}
+            onClick={(e) => {
+              e.stopPropagation();
+              handleSelectOption(option);
+              setIsopen(false);
+            }}
+          >
+            {option.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
